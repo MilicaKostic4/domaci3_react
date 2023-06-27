8b1ffbae8a229c7a8a6a98f7876e686224d4152f
MilicaKostic4 <mica.kostiic@gmail.com>
2023-06-27 19:53:57 +0200

    Finalne izmene

--- src/components/Omiljeni.jsx
+++ src/components/Omiljeni.jsx
@@ -4,6 +4,10 @@ import Restoran from './Restoran'
 const Omiljeni = ({restorani, ukloni, rezervisi, pretraga}) => {
   return (
     <div className='restoran-container'>
+      <div className="poruka">
+        <h1>Ovo su Vaši omiljeni restorani:</h1>
+      </div>
+
       {pretraga===""? (restorani.map((r) => (
             <Restoran key={r.id} restoran={r} favourite={0} ukloni={ukloni} rezervisi={rezervisi}/>)))
             :
